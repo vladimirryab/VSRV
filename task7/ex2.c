0f4ec73fd81f75150b348202ecc2a1ec0125b42a
vladimirryab <vladimirr1003@yandex.ru>
2025-11-16 03:07:02 +0300

    commit 16.11.2025

---
+++ task7/ex2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World");
+  int result;
+   int v1, v2;
+   v1 = 121;
+   v2 = 0;
+   result = v1/v2;
+   printf("Result of Divide by Zero is %d\n", result);
+    return 0;
+}
